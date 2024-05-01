ee16ee98c074fca7af0503a157aa635d0390c88e
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-05-01 14:05:57 +0530

    Update InsurancePolicyController.cs

--- Backend/InsurancePolicyManagement/Controllers/InsurancePolicyController.cs
+++ Backend/InsurancePolicyManagement/Controllers/InsurancePolicyController.cs
@@ -34,58 +34,6 @@ namespace InsurancePolicyManagement.Controllers
 
         }
 
-
-        // [HttpPut]
-        // [Route("update-policy")]
-        // public async Task<IActionResult> UpdateInsurancePolicy([FromBody] InsurancePolicyViewModel model)
-        // {
-        //     var policy = await _insurancePolicyService.UpdateInsurancePolicy(model);
-        //     if (policy == null)
-        //     {
-        //         return StatusCode(StatusCodes.Status500InternalServerError, new Response
-        //         { Status = "Error", Message = $"Insurance Policy With Id = {model.PolicyId} cannot be found" });
-        //     }
-        //     else
-        //     {
-        //         var result = await _insurancePolicyService.UpdateInsurancePolicy(model);
-        //         return Ok(new Response { Status = "Success", Message = "Insurance Policy updated successfully!" });
-        //     }
-        // }
-
-        // [HttpDelete]
-        // [Route("delete-policy")]
-        // public async Task<IActionResult> DeleteInsurancePolicy(long id)
-        // {
-        //     var policy = await _insurancePolicyService.GetInsurancePolicyById(id);
-        //     if (policy == null)
-        //     {
-        //         return StatusCode(StatusCodes.Status500InternalServerError, new Response
-        //         { Status = "Error", Message = $"Insurance Policy With Id = {id} cannot be found" });
-        //     }
-        //     else
-        //     {
-        //         var result = await _insurancePolicyService.DeleteInsurancePolicyById(id);
-        //         return Ok(new Response { Status = "Success", Message = "Insurance policy deleted successfully!" });
-        //     }
-        // }
-
-
-        // [HttpGet]
-        // [Route("get-policy-by-id")]
-        // public async Task<IActionResult> GetInsurancePolicyById(long id)
-        // {
-        //     var policy = await _insurancePolicyService.GetInsurancePolicyById(id);
-        //     if (policy == null)
-        //     {
-        //         return StatusCode(StatusCodes.Status500InternalServerError, new Response
-        //         { Status = "Error", Message = $"Insurance Policy With Id = {id} cannot be found" });
-        //     }
-        //     else
-        //     {
-        //         return Ok(policy);
-        //     }
-        // }
-
         [HttpGet]
         [Route("get-all-policies")]
         public async Task<IEnumerable<InsurancePolicy>> GetAllPolicies()
